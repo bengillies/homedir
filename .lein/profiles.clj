09dfb4feb20ba06f45fac60bd27a080c59457097
Ben Gillies <bengillies@gmail.com>
2013-06-21 15:54:59 +0100

    Add leiningen config

---
+++ .lein/profiles.clj
@@ -0,0 +1,8 @@
+{:user {:plugins [[lein-noir "1.2.1"]
+                  [lein-exec "0.2.1"]
+                  [lein-cascalog "0.1.0"]
+                  [lein-midje "2.0.3"]
+                  [radagast "1.1.1"]
+                  [lein-drip "0.1.1-SNAPSHOT"]
+                  [lein-ancient "0.3.1"]
+                  [lein-kibit "0.0.7"]]}}
