29e54db1707d53304f10e0106cf4baeefce12b69
Ben Gillies <bengillies@gmail.com>
2017-04-16 12:35:57 +0100

    Update version numbers

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,10 @@
-{:user {:plugins [[lein-noir "1.2.1"]
-                  [lein-exec "0.2.1"]
-                  [lein-cascalog "0.1.0"]
-                  [lein-midje "2.0.3"]
-                  [radagast "1.1.1"]
-                  [lein-drip "0.1.1-SNAPSHOT"]
-                  [lein-ancient "0.3.1"]
-                  [lein-kibit "0.0.8"]
-                  [lein-try "0.4.1"]]}}
+{:user {:plugins
+        [[lein-noir "1.2.1"]
+         [lein-exec "0.2.1"]
+         [lein-cascalog "0.1.0"]
+         [lein-midje "3.1.3"]
+         [radagast "1.1.1"]
+         [lein-drip "0.1.1-SNAPSHOT"]
+         [lein-ancient "0.6.7"]
+         [lein-kibit "0.0.8"]
+         [lein-try "0.4.1"]]}}
