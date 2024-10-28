136bcca2d66c1db61ea705c56cf16ea3600491d7
Ben Gillies <bengillies@gmail.com>
2024-10-28 23:05:32 +0000

    More autocmds converted

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,12 +1,12 @@
 -- Utility function for creating autocommands
 local function augroup(name)
-  return vim.api.nvim_create_augroup("custom_" .. name, { clear = true })
+  return vim.api.nvim_create_augroup('custom_' .. name, { clear = true })
 end
 
 -- use indents of 4 spaces apart from ruby, handlebars and coffeescript, js where we want 2
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("indentation"),
-  pattern = { "ruby", "coffee", "javascript", "javascriptreact", "typescript", "typescriptreact", "mustache" },
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup('indentation'),
+  pattern = { 'ruby', 'coffee', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'mustache' },
   callback = function()
     vim.opt_local.softtabstop = 2
     vim.opt_local.shiftwidth = 2
@@ -16,56 +16,56 @@ vim.api.nvim_create_autocmd("FileType", {
 
 -- tabs instead of spaces
 -- apart from python, Ruby, Handlebars and CoffeeScript where we want spaces.
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("expandtab"),
-  pattern = { "python", "ruby", "coffee", "javascript", "javascriptreact", "typescript", "typescriptreact", "mustache" },
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup('expandtab'),
+  pattern = { 'python', 'ruby', 'coffee', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'mustache' },
   callback = function()
     vim.opt_local.expandtab = true
   end,
 })
 
 -- JSON file settings
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  group = augroup("json_files"),
-  pattern = "*.json",
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+  group = augroup('json_files'),
+  pattern = '*.json',
   callback = function()
-    vim.opt_local.filetype = "json"
-    vim.opt_local.syntax = "javascript"
+    vim.opt_local.filetype = 'json'
+    vim.opt_local.syntax = 'javascript'
   end,
 })
 
 -- JSON5 file settings
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  group = augroup("json5_files"),
-  pattern = "*.json5",
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+  group = augroup('json5_files'),
+  pattern = '*.json5',
   callback = function()
-    vim.opt_local.filetype = "javascript"
-    vim.opt_local.syntax = "javascript"
+    vim.opt_local.filetype = 'javascript'
+    vim.opt_local.syntax = 'javascript'
   end,
 })
 
 -- ES6 files
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  group = augroup("es6_files"),
-  pattern = "*.es6",
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  group = augroup('es6_files'),
+  pattern = '*.es6',
   callback = function()
-    vim.opt_local.filetype = "javascript"
+    vim.opt_local.filetype = 'javascript'
   end,
 })
 
 -- Markdown files
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  group = augroup("markdown_files"),
-  pattern = "*.md",
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  group = augroup('markdown_files'),
+  pattern = '*.md',
   callback = function()
-    vim.opt_local.filetype = "markdown"
+    vim.opt_local.filetype = 'markdown'
   end,
 })
 
 -- Spelling mistakes
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("spelling_fixes"),
-  pattern = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup('spelling_fixes'),
+  pattern = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
   callback = function()
     vim.keymap.set('ia', 'retrun', 'return', {})
     vim.keymap.set('ia', 'lenght', 'length', {})
@@ -73,82 +73,94 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 -- Code folding settings
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("folding"),
-  pattern = { "javascript", "javascriptreact", "typescript", "typescriptreact", "css", "scss" },
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup('folding'),
+  pattern = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'css', 'scss' },
   callback = function()
-    vim.opt_local.foldmethod = "marker"
-    vim.opt_local.foldmarker = "{,}"
+    vim.opt_local.foldmethod = 'marker'
+    vim.opt_local.foldmarker = '{,}'
     -- Note: We'll define MarkerFoldText function separately
   end,
 })
 
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("indent_folding"),
-  pattern = { "python", "html" },
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup('indent_folding'),
+  pattern = { 'python', 'html' },
   callback = function()
-    vim.opt_local.foldmethod = "indent"
+    vim.opt_local.foldmethod = 'indent'
   end,
 })
 
 -- Remove indenting for JS/Ruby
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("indent_override"),
-  pattern = { "ruby", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup('indent_override'),
+  pattern = { 'ruby', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
   callback = function()
-    vim.opt_local.indentexpr = "cindent"
+    vim.opt_local.indentexpr = 'cindent'
   end,
 })
 
 -- Relative number toggle based on focus/mode
-vim.api.nvim_create_autocmd("FocusLost", {
-  group = augroup("numbertoggle"),
+vim.api.nvim_create_autocmd('FocusLost', {
+  group = augroup('numbertoggle'),
   callback = function()
     vim.opt.number = true
     vim.opt.relativenumber = false
   end,
 })
 
-vim.api.nvim_create_autocmd("FocusGained", {
-  group = augroup("numbertoggle"),
+vim.api.nvim_create_autocmd('FocusGained', {
+  group = augroup('numbertoggle'),
   callback = function()
     vim.opt.relativenumber = true
   end,
 })
 
-vim.api.nvim_create_autocmd("InsertEnter", {
-  group = augroup("numbertoggle"),
+vim.api.nvim_create_autocmd('InsertEnter', {
+  group = augroup('numbertoggle'),
   callback = function()
     vim.opt.number = true
     vim.opt.relativenumber = false
   end,
 })
 
-vim.api.nvim_create_autocmd("InsertLeave", {
-  group = augroup("numbertoggle"),
+vim.api.nvim_create_autocmd('InsertLeave', {
+  group = augroup('numbertoggle'),
   callback = function()
     vim.opt.relativenumber = true
   end,
 })
 
 -- Load previous session if it exists and then delete it
-vim.api.nvim_create_autocmd("VimEnter", {
-  group = augroup("session_load"),
+vim.api.nvim_create_autocmd('VimEnter', {
+  group = augroup('session_load'),
   callback = function()
-    local session_file = "Session.vim"
+    local session_file = 'Session.vim'
     if vim.fn.filereadable(session_file) == 1 then
-      vim.cmd("source " .. session_file)
+      vim.cmd('source ' .. session_file)
       vim.fn.delete(session_file)
     end
   end,
 })
 
 -- Pipe JSON through jq to format
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "json",
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'json',
   callback = function()
-    vim.keymap.set("v", "=", ":!jq .<CR>", { buffer = true, noremap = true })
+    vim.keymap.set('v', '=', ':!jq .<CR>', { buffer = true })
   end,
+})
+
+-- Use prettier to format JS/TS files
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {'javascript', 'javascriptreact', 'typescript', 'typescriptreact'},
+  callback = function()
+    vim.keymap.set('n', '==', function()
+      vim.cmd(':!npx prettier -w ' .. vim.fn.expand('%'))
+      vim.cmd('edit')
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<CR>', true, true, true), 'n', false)
+    end, { buffer = true })
+  end
 })
 
 -- Define the MarkerFoldText function
@@ -185,8 +197,8 @@ _G.MarkerFoldText = function()
 
   -- Add the line count
   local lines_count = vim.v.foldend - vim.v.foldstart + 1
-  local info = " " .. lines_count .. " lines"
-  sub = sub .. string.rep(" ", 100)  -- padding
+  local info = ' ' .. lines_count .. ' lines'
+  sub = sub .. string.rep(' ', 100)  -- padding
 
   -- Calculate width adjustments for line numbers and fold column
   local num_w = vim.wo.number and vim.wo.numberwidth or 0
@@ -203,7 +215,7 @@ _G.MarkerFoldText = function()
 end
 
 -- Set up the fold text
-vim.opt.foldtext = "v:lua.MarkerFoldText()"
+vim.opt.foldtext = 'v:lua.MarkerFoldText()'
 
 -- easier quickfix/loclist navigation
 
@@ -218,32 +230,32 @@ local function either_ql_buffer(qfix, loc)
 end
 
 -- Set up autocmd group for quickfix window mappings
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "qf",
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'qf',
   callback = function()
     -- Local mappings for quickfix/location list buffer
     local opts = { buffer = true, noremap = true, silent = true }
 
-    vim.keymap.set("n", "<CR>", function()
-      either_ql_buffer(":cc", ":ll")
+    vim.keymap.set('n', '<CR>', function()
+      either_ql_buffer(':cc', ':ll')
     end, opts)
 
-    vim.keymap.set("n", "<Esc>", function()
-      either_ql_buffer(":ccl", ":lcl")
-      vim.cmd("wincmd p")
+    vim.keymap.set('n', '<Esc>', function()
+      either_ql_buffer(':ccl', ':lcl')
+      vim.cmd('wincmd p')
     end, opts)
 
-    vim.keymap.set("n", "<S-j>", function()
-      either_ql_buffer("cnext", "lnext")
-      vim.cmd("wincmd p")
+    vim.keymap.set('n', '<S-j>', function()
+      either_ql_buffer('cnext', 'lnext')
+      vim.cmd('wincmd p')
     end, opts)
 
-    vim.keymap.set("n", "<S-k>", function()
-      either_ql_buffer("cprevious", "lprevious")
-      vim.cmd("wincmd p")
+    vim.keymap.set('n', '<S-k>', function()
+      either_ql_buffer('cprevious', 'lprevious')
+      vim.cmd('wincmd p')
     end, opts)
   end,
 })
 
 -- Global mapping to refocus on quickfix/location list
-vim.keymap.set("n", "<Space>q", "<C-w>p", { noremap = true, silent = true })
+vim.keymap.set('n', '<Space>q', '<C-w>p', { noremap = true, silent = true })
