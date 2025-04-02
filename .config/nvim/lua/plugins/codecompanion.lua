73555df9c3263cad766c14097e9a4274504c10e8
Ben Gillies <bengillies@gmail.com>
2025-04-02 09:54:51 +0100

    Add mcphub

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -3,12 +3,22 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
+    "ravitemer/mcphub.nvim",
   },
   config = true,
   opts = {
     strategies = {
       chat = {
         adapter = "copilot",
+        tools = {
+          ["mcp"] = {
+            callback = function() return require("mcphub.extensions.codecompanion") end,
+            description = "Call tools and resources from the MCP Servers",
+            opts = {
+              requires_approval = true,
+            }
+          }
+        }
       },
       inline = {
         adapter = "copilot",
